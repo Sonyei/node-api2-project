e7cabba2db8b24ea797cd010682e7b6a619081ec
Angelika Blackburn <esonyei@gmail.com>
2021-02-18 12:16:18 -0500

    POSTING new posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -11,7 +11,9 @@ router.get("/api/posts", (req, res) => {
 		})
 		.catch((err) => {
 			console.log(err);
-			res.status(500).json({ message: "Error retrieving posts." });
+			res
+				.status(500)
+				.json({ message: "The posts information could not be retrieved" });
 		});
 });
 
@@ -22,14 +24,35 @@ router.get("/api/posts/:id", (req, res) => {
 			if (post) {
 				res.status(200).json(post);
 			} else {
-				res.status(404).json({ message: "Post not found." });
+				res
+					.status(404)
+					.json({ message: "The post with the specified ID does not exist" });
 			}
 		})
 		.catch((err) => {
 			console.log(err);
 			res
 				.status(500)
-				.json({ message: "Something went wrong. Please try again later." });
+				.json({ message: "The posts information could not be retrieved" });
+		});
+});
+
+router.post("/api/posts", (req, res) => {
+	if (!req.body.title || !req.body.contents) {
+		return res.status(400).json({
+			message: "Please provide title and contents for the post",
+		});
+	}
+	posts
+		.insert(req.body)
+		.then((post) => {
+			res.status(201).json(post);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				message: "There was an error while saving the post to the database.",
+			});
 		});
 });
 
