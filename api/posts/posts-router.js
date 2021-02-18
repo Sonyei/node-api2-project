d1e4adbefa04df682645c18805aaa8af9ce3c2f0
Angelika Blackburn <esonyei@gmail.com>
2021-02-18 12:32:41 -0500

    DELETE posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -81,4 +81,26 @@ router.put("/api/posts/:id", (req, res) => {
 		});
 });
 
+router.delete("/api/posts/:id", (req, res) => {
+	posts
+		.remove(req.params.id)
+		.then((post) => {
+			if (post > 0) {
+				res.status(200).json({
+					message: "Post has been deleted.",
+				});
+			} else {
+				res.status(404).json({
+					message: "The post with the specified ID does not exist.",
+				});
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json({
+				message: "The post could not be removed.",
+			});
+		});
+});
+
 module.exports = router;
