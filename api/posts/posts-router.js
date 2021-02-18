ed079e065ec81772ae4d8e498f2f0fdb07c7ba9a
Angelika Blackburn <esonyei@gmail.com>
2021-02-18 12:10:13 -0500

    fetching by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -15,4 +15,22 @@ router.get("/api/posts", (req, res) => {
 		});
 });
 
+router.get("/api/posts/:id", (req, res) => {
+	posts
+		.findById(req.params.id)
+		.then((post) => {
+			if (post) {
+				res.status(200).json(post);
+			} else {
+				res.status(404).json({ message: "Post not found." });
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+			res
+				.status(500)
+				.json({ message: "Something went wrong. Please try again later." });
+		});
+});
+
 module.exports = router;
